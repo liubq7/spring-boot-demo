3e2dd98ea9845b54a9ec87feb22c7503946dd1c6
liubq7 <liubq97@gmail.com>
2022-01-22 13:58:52 +0800

    simple shiro auth

--- src/main/java/com/example/studentmanagement/api/StudentController.java
+++ src/main/java/com/example/studentmanagement/api/StudentController.java
@@ -5,6 +5,7 @@ import com.example.studentmanagement.exceptions.StudentNonExistException;
 import com.example.studentmanagement.exceptions.UniversityClassInvalidException;
 import com.example.studentmanagement.model.Student;
 import com.example.studentmanagement.service.StudentService;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,11 +25,13 @@ public class StudentController {
     }
 
     @GetMapping
+    @RequiresPermissions("student:read")
     public List<Student> getAllStudent() {
         return studentService.getAllStudents();
     }
 
     @PutMapping
+    @RequiresPermissions("student:write")
     public ResponseEntity<String> updateStudent(@RequestBody Student student) {
         try {
             studentService.updateStudent(student);
@@ -39,24 +42,28 @@ public class StudentController {
     }
 
     @GetMapping("/name")
+    @RequiresPermissions("student:read")
     // localhost:8080/api/student/name?name=tom
     public List<Student> getStudentsByName(@RequestParam String name) {
         return studentService.getStudentsByName(name);
     }
 
     @GetMapping("/with_name")
+    @RequiresPermissions("student:read")
     // localhost:8080/api/student/with_name?str=t
     public List<Student> getStudentsWithName(@RequestParam String str) {
         return studentService.getStudentsWithName(str);
     }
 
     @GetMapping("/class")
+    @RequiresPermissions("student:read")
     // localhost:8080/api/student/class?year=2022&number=1
     public List<Student> getStudentsInClass(@RequestParam Integer year, @RequestParam Integer number) {
         return studentService.getStudentsInClass(year, number);
     }
 
     @PostMapping("/register")
+    @RequiresPermissions("student:write")
     public ResponseEntity<String> registerStudent(@RequestBody Student student) {
         try {
             Student savedStudent = studentService.addStudent(student);
@@ -67,6 +74,7 @@ public class StudentController {
     }
 
     @PostMapping("/assignclass/{sid}/{cid}")
+    @RequiresPermissions("student:write")
     public ResponseEntity<String> assignClass(@PathVariable("sid") Long studentId, @PathVariable("cid") Long classId) {
         try {
             Student student = studentService.assignClass(studentId, classId);

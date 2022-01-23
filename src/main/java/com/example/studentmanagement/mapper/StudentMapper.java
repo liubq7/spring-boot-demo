7c4202df1574805861572c4c056058c8b5556538
liubq7 <liubq97@gmail.com>
2022-01-23 10:37:55 +0800

    add class mapper in getStudentsInClass

--- src/main/java/com/example/studentmanagement/mapper/StudentMapper.java
+++ src/main/java/com/example/studentmanagement/mapper/StudentMapper.java
@@ -17,10 +17,15 @@ public interface StudentMapper {
     })
     List<Student> getStudentsWithName(@Param("str") String str);
 
-    @Select("SELECT * FROM university_class WHERE id = #{id}")
-    UniversityClass selectClass(@Param("id") Long id);
-
     @Select("SELECT * FROM student WHERE university_class_id IN" +
             "(SELECT id FROM university_class WHERE year = #{year} AND number = #{number})")
+    @Results(value = {
+            @Result(property = "id", column = "id"),
+            @Result(property = "name", column = "name"),
+            @Result(property = "universityClass", column = "university_class_id", one = @One(select = "selectClass"))
+    })
     List<Student> getStudentsInClass(@Param("year") Integer year, @Param("number") Integer number);
+
+    @Select("SELECT * FROM university_class WHERE id = #{id}")
+    UniversityClass selectClass(@Param("id") Long id);
 }
